6994889bbf6476d2046555d33b6ebb25f0efdad1
skuzmukov <s.kuzmukov@gmail.com>
2020-03-12 20:24:40 +0500

    task 1209

--- src/Task_1209.java
+++ src/Task_1209.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 
 public class Task_1209 {
+    public static void main(String[] args) throws IOException {
+        StreamTokenizer in = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        in.nextToken();
+        int f = (int) in.nval;
+        for (int i = 0; i < f; i++) {
+            in.nextToken();
+            System.out.println(Math.sqrt(8 * in.nval - 7) % 1 == 0 ? 1 : 0);
+        }
+    }
 }
